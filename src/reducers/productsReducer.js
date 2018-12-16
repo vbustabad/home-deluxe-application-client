5a64126f05940bbb2d7d07d78b2acea044912d4c
Veronica Bustabad <vbustabad@gmail.com>
2018-12-15 20:17:04 -0800

    Added GET_INDIVIDUAL_PRODUCT_INFORMATION case statement to productsReducer

--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,10 +1,15 @@
-export default function productsReducer(state = [], action) {
+const initialState = {products: [], product: {}};
+
+export default function productsReducer(state = initialState, action) {
     switch (action.type) {
       case "GET_PRODUCTS_INFORMATION":
-        return  action.products;
+        return  {...state, products: action.products};
+
+      case "GET_INDIVIDUAL_PRODUCT_INFORMATION":
+        return  {...state, product: action.product};
 
       case "ADD_PRODUCT":
-        return  state.concat(action.product);
+        return  {...state, products: state.products.concat(action.product) };
   
       default:
         return state;
