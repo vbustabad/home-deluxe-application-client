11acd1a6e6cd916f73d1f82e8838df3d4b7fb1fe
Veronica Bustabad <vbustabad@gmail.com>
2018-12-15 20:10:50 -0800

    Changed route for Product show page to IndividualProductContainer component

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,7 +26,7 @@ class App extends Component {
                 <Route exact path="/login" component={Login}/>
                 <Route exact path="/order" component={Order}/>
                 <Route exact path="/products" component={Products}/>
-                <Route exact path="/products/:id" component={ProductCard}/>
+                <Route exact path="/products/:id" component={IndividualProductContainer}/>
                 <Route exact path="/products/new" component={ProductForm}/> 
                 <Route exact path="/profile" component={Profile}/>
                 <Route exact path="/shopping-cart" component={ShoppingCart}/>
